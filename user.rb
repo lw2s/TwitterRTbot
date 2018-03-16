7bcec93fd055f51d25664eca6007aa4a2c0a6cf1
R Fujii <sfnnnk@gmail.com>
2018-03-16 12:06:58 +0900

    change user.rb

--- user.rb
+++ user.rb
@@ -1,25 +1,28 @@
 require "twitter"
 
 client = Twitter::REST::Client.new do |config|
-  config.consumer_key           = "yx8eFVXTRW7BDc2vsLY4vKpV7"
-  config.consumer_secret        = "rMcS9dVm6V5Oquo9H28ZLqZiKxU8vYZc2l4WrcSR1yYPhYFEuA"
-  config.access_token           = "858336626136588289-JSYIJbiT494FryruNNUog9i1W1TsjD1"
-  config.access_token_secret    = "bqCj1g8pG6hXWzwrwRWiCyeto2bpaKZGYtFN3FzmwXDHr"
+  config.consumer_key           = ""
+  config.consumer_secret        = ""
+  config.access_token           = ""
+  config.access_token_secret    = ""
 end
 
 loop do
 client.home_timeline(count: 200, include_rts: false).each do |timeline|
-  a = []
-  
-  if (a.include?(timeline.id))
+  tl_id = []
+
+  # 配列にすでにあるtimeline.idがあれば次へなければtl_idに格納
+  if (tl_id.inあるclude?(timeline.id))
     next
   end
-  a << (timeline.id)
-  
-  if (/^(.*あああ.*)$/) =~ timeline.text
-    #client.retweet(timeline.id) rescue nil
-    client.update("@",timeline.id, timeline.text)
+  tl_id << (timeline.id)
+
+　# タイムラインにデレステという単語が見つかればリツイート
+  if (/^(.*デレステ.*)$/) =~ timeline.text
+    client.retweet(timeline.id) rescue nil
   end
 end
+
 sleep(60)
+
 end
